97bffef7ca588594de27c89f9abcf6643c7cab86
Ledi03 <128150475+Ledi03@users.noreply.github.com>
2023-04-06 14:11:59 +0200

    Update square.c

--- square.c
+++ square.c
@@ -1,25 +1,25 @@
 //llamce22
 #include <stdio.h>
 
-float calculate_area(float length) {
+double calculate_area(double length) {
     return length * length;
 }
 
-float calculate_perimeter(float length) {
+double calculate_perimeter(double length) {
     return 4 * length;
 }
 
 int main() {
-    float length, area, perimeter;
+    double length, area, perimeter;
 
     printf("Enter the length of the square: ");
-    scanf("%f", &length);
+    scanf("%lf", &length);
 
     area = calculate_area(length);
     perimeter = calculate_perimeter(length);
 
-    printf("Area of the square is: %.2f\n", area);
-    printf("Perimeter of the square is: %.2f\n", perimeter);
+    printf("Area of the square is: %.2lf\n", area);
+    printf("Perimeter of the square is: %.2lf\n", perimeter);
 
     return 0;
 }
