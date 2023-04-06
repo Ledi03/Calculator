cdd5d919fd1ddf72e87f7cd76a6cfc41711abd61
Ledi03 <128150475+Ledi03@users.noreply.github.com>
2023-04-06 14:14:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,55 +1,56 @@
+//llamce22
 #include <stdio.h>
 #include <math.h>
 
-float addition(float num1, float num2) {
-    float result = num1 + num2;
-    printf("Result: %.2f\n", result);
+double addition(double num1, double num2) {
+    double result = num1 + num2;
+    printf("Result: %.2lf\n", result);
     return result;
 }
 
-float subtraction(float num1, float num2) {
-    float result = num1 - num2;
-    printf("Result: %.2f\n", result);
+double subtraction(double num1, double num2) {
+    double result = num1 - num2;
+    printf("Result: %.2lf\n", result);
     return result;
 }
 
-float multiplication(float num1, float num2) {
-    float result = num1 * num2;
-    printf("Result: %.2f\n", result);
+double multiplication(double num1, double num2) {
+    double result = num1 * num2;
+    printf("Result: %.2lf\n", result);
     return result;
 }
 
-float division(float num1, float num2) {
+double division(double num1, double num2) {
     if (num2 == 0) {
         printf("Error: cannot divide by zero\n");
         return 0;
     } else {
-        float result = num1 / num2;
-        printf("Result: %.2f\n", result);
+        double result = num1 / num2;
+        printf("Result: %.2lf\n", result);
         return result;
     }
 }
 
-float squareRoot(float num1) {
+double squareRoot(double num1) {
     if (num1 < 0) {
         printf("Error: cannot find square root of a negative number\n");
         return 0;
     } else {
-        float result = sqrt(num1);
-        printf("Result: %.2f\n", result);
+        double result = sqrt(num1);
+        printf("Result: %.2lf\n", result);
         return result;
     }
 }
 
-float power(float num1, float num2) {
-    float result = pow(num1, num2);
-    printf("Result: %.2f\n", result);
+double power(double num1, double num2) {
+    double result = pow(num1, num2);
+    printf("Result: %.2lf\n", result);
     return result;
 }
 
 int main() {
     int choice;
-    float num1, num2, result;
+    double num1, num2, result;
 
     printf("Select an operation:\n");
     printf("1. Addition\n");
@@ -63,32 +64,32 @@ int main() {
     switch (choice) {
         case 1:
             printf("Enter two numbers to add:\n");
-            scanf("%f %f", &num1, &num2);
+            scanf("%lf %lf", &num1, &num2);
             result = addition(num1, num2);
             break;
         case 2:
             printf("Enter two numbers to subtract:\n");
-            scanf("%f %f", &num1, &num2);
+            scanf("%lf %lf", &num1, &num2);
             result = subtraction(num1, num2);
             break;
         case 3:
             printf("Enter two numbers to multiply:\n");
-            scanf("%f %f", &num1, &num2);
+            scanf("%lf %lf", &num1, &num2);
             result = multiplication(num1, num2);
             break;
         case 4:
             printf("Enter two numbers to divide:\n");
-            scanf("%f %f", &num1, &num2);
+            scanf("%lf %lf", &num1, &num2);
             result = division(num1, num2);
             break;
         case 5:
             printf("Enter a number to find its square root:\n");
-            scanf("%f", &num1);
+            scanf("%lf", &num1);
             result = squareRoot(num1);
             break;
         case 6:
             printf("Enter a number and its power:\n");
-            scanf("%f %f", &num1, &num2);
+            scanf("%lf %lf", &num1, &num2);
             result = power(num1, num2);
             break;
     }
